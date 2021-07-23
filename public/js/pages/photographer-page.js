91f922304d4e811191fe28404021514c49121c4c
faiza <faiza.laaroussi2016@gmail.com>
2021-07-23 21:02:34 +0200

    Chragement des données de la page photographer

--- public/js/pages/photographer-page.js
+++ public/js/pages/photographer-page.js
@@ -1,6 +1,6 @@
 //Etape 1: Récuperation des données à partir du fichier jason.
 const fetchData = async () =>{
-   let url ="/data/FishEyeData.json";
+   let url ="datas/FishEyeData.json";
    // await interreompt l'execution de la fonction async
    //attend la résolution de la promesse passée Promise. 
    //La fonction asynchrone reprend ensuite puis renvoie la valeur de résolution.
