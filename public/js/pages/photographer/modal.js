d872e1ed85b4bb55d22565e4f2e3487d84976873
faiza <faiza.laaroussi2016@gmail.com>
2021-08-02 11:14:19 +0200

    Page photographe : modal

--- public/js/pages/photographer/modal.js
+++ public/js/pages/photographer/modal.js
@@ -1,24 +1,105 @@
 
 
-export default class Modal{
+export default class Modal {
 
-    modalBtn = document.querySelectorAll(".modal-btn");
-    modalbg = document.querySelector(".bground");
+  modalBtn = document.querySelectorAll(".modal-btn");
+  modalbg = document.querySelector(".bground");
 
-  initModal(){  
-      // Open modal event
-      this.modalBtn.forEach( btn => {
-          btn.addEventListener("click", event => {
-            this.openModal();
-            console.log(this.modalBtn);
-        })
+  initModal() {
+    // Open modal event
+    this.modalBtn.forEach(btn => {
+      btn.addEventListener("click", event => {
+        this.openModal();
       })
+    });
+    //Initialisation des controles.
+    this.controleForm();
+  }
+
+  // Open modal form
+  openModal() {
+    this.modalbg.style.display = "block";
+  }
+
+  // Open modal form
+  //Fermer le formulaire = modal.
+  modalClose = document.querySelector(".close");
+  doCloseModal() {
+    this.modalClose.addEventListener("click", event => {
+      modalbg.style.display = "none";
+    })
   }
   
-    // Open modal form
-    openModal(){
-        this.modalbg.style.display = "block";
+  //control & validation Modal
+
+  //DOM Elements.
+  firstNameElt  = document.getElementById("first-name");
+  lastNameElt   = document.getElementById("last-name");
+  emailElt      = document.getElementById("email")
+
+  controleForm() {
+    this.firstNameElt.addEventListener("input", event => {
+      this.validatePrenom(this.firstNameElt);
+    })
+    this.lastNameElt.addEventListener("input", event => {
+      this.validatePrenom(this.lastNameElt);
+    })
+    this.emailElt.addEventListener("input", event => {
+      this.validateEmail(this.emailElt);
+    })
+  }
+
+  validatePrenom(prenomElt) {
+      let prenom = prenomElt.value;
+      //RegExp :ecrire une chaine avec au moins 3 lettres maj ou bien min.
+      let regexNomPrenom = new RegExp("^[a-zA-Z]{2,}$");
+      let msgErreurPrenom = document.getElementById("first_error");
+      // le message d'erreur doit etre vide à chaque validation à chaque click sur le boutton C'est parti.
+      msgErreurPrenom.textContent = "";
+      // annuler le border.
+      prenomElt.dataset.errorVisible = "false";
+
+      if (regexNomPrenom.test(prenom) === false) {
+        msgErreurPrenom.textContent = "Saisissez un prénom qui contient au moins deux caractères alphabétiques";
+        prenomElt.dataset.errorVisible = "true";
+        return false;
+      }
+      return true;
+  }
+
+  
+
+  validateEmail(emailElt) {
+    let email = emailElt.value;
+    let msgErrorEmail = document.getElementById("email_error");
+    let regexEmail = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+    msgErrorEmail.textContent = "";
+    emailElt.dataset.errorVisible = "false";
+
+    if (regexEmail.test(email) === false) {
+      msgErrorEmail.textContent = "Saisissez une adresse électronique valide";
+      emailElt.dataset.errorVisible = "true";
+      return false;
     }
+    return true;
+  }
+
+  validateMessage(msglElt) {
+    let msg = msgElt.value;
+    let msgErrorMsg = document.getElementById("msg_error");
+    msgErrorMsg.textContent = "";
+    msgElt.dataset.errorVisible = "false";
+
+    if (typeof msg !== 'string') {
+      msgErrorMsg.textContent = "Saisissez une chaine de caractères";
+      msglElt.dataset.errorVisible = "true";
+      return false;
+    }
+    return true;
+  }
 
 }
+
+
+
 
--- public/js/pages/photographer/photographer-page.js
+++ public/js/pages/photographer/photographer-page.js
@@ -3,19 +3,26 @@
 //on creer un element 'article'ou on fait apparaitre le photographe selectionné par la l'appele de 
 //la fonction getTemplatePhotographerProfil
 import Modal from "./Modal.js"
+
+
 export default class PhotographerPage {
 
-  displayProfil(photographers) {
-                  
+   displayProfil(photographers) {
+
       let ident = this.getPhotographerId();
-
-      let phtographeSelectionne = photographers.filter(currentPhotographer => currentPhotographer.id == ident);
-      
-
-
       // photographer_profil
       let elt = document.getElementById("photographer_profil");
-      elt.innerHTML = this.getTemplatePhotographerProfil(phtographeSelectionne[0]);
+      let phtographeSelectionne = photographers.filter(currentPhotographer => currentPhotographer.id == ident);
+
+      // si on a un id non connu on affiche un message d'erreur
+      //donc ajouter la condition suivante :phtographeSelectionne[0].length:au moins un photographe existe.
+      if(phtographeSelectionne[0]){         
+         elt.innerHTML = this.getTemplatePhotographerProfil(phtographeSelectionne[0]);
+      }else{
+         elt.innerHTML = "Veuillez sélectionner un photographer";
+         return;
+      }
+      
       //ajout name du photographe dans le modal.
       document.getElementById("modal_photographer_name").textContent = phtographeSelectionne[0].name;
       // initialize le modal
@@ -23,23 +30,25 @@ export default class PhotographerPage {
       modal.initModal();
    }
 
-
    //Etatpe 2:Création template d'un photographe à travers la définition de la fct  getTemplatePhotographer.
    //x=phtographeSelectionne[0].
    getTemplatePhotographerProfil(x) {
       let template = `  
          <article class="photographer-card photographer-card__direction"> 
             <div class="photographer-infos">
-               <h2>${x.name}</h2>
-               <p class="localisation">${x.city}</p>
-               <p class="tagline">${x.tagline}</p>
-               <ul class="filtres">
-                  ${x.tags.map(tag => `<li><a class="tag">#${tag}</a></li>`).join(" ")}
-               </ul>  
-            </div>
+               <div>   
+                  <h2>${x.name}</h2>
+                  <p class="localisation">${x.city}</p>
+                  <p class="tagline">${x.tagline}</p>
+                  <ul class="filtres">
+                     ${x.tags.map(tag => `<li><a class="tag">#${tag}</a></li>`).join(" ")}
+                  </ul>  
+               </div>   
+               <div>
+                  <button class="btn modal-btn">Contactez-moi</button>
+               </div>
 
-            <button class="btn modal-btn">Contactez-moi</button>
-         
+            </div>
             <figure>
                <img src="./public/images/Photographers/${x.portrait}" alt="${x.alt}"/>
             </figure>
@@ -49,47 +58,40 @@ export default class PhotographerPage {
       return template;
    }
 
-/**
- * Traitement media par photographe
- * @param {*} listMedia 
- */
+   /**
+    * Traitement media par photographe
+    * @param {*} listMedia 
+    */
 
-   displayMedia(listMedia){
+   displayMedia(listMedia) {
 
 
       let elt = document.getElementById("photographer_media");
       elt.innerHTML = "";
-
       let ident = this.getPhotographerId();
+      let phtographerMedia = listMedia.filter(currentMedia => currentMedia.photographerId == ident);
 
-      let phtographerMedia = listMedia.filter(currentMedia=> currentMedia.photographerId == ident);
-      
-
-
-
-      phtographerMedia.forEach(currentMedia =>{
-      
+      phtographerMedia.forEach(currentMedia => {
          let article = document.createElement("article");
-         article.className = "media-card";  
-         
+         article.className = "media-card";
+
          //Afficher soit image soit video
-            //
-         if(currentMedia.hasOwnProperty("image") ){
+         if (currentMedia.hasOwnProperty("image")) {
             article.innerHTML = this.getTemplateImage(currentMedia);
-         }else if (currentMedia.hasOwnProperty("video") ){
-            article.innerHTML = this.getTemplateVideo(currentMedia);  
-         }else{
+         } else if (currentMedia.hasOwnProperty("video")) {
+            article.innerHTML = this.getTemplateVideo(currentMedia);
+         } else {
             article.innerHTML = "Media non disponible";
          }
-         
-         elt.appendChild(article)      
+
+         elt.appendChild(article)
       })
       //l'ajout d'un like lorsqu'on click sur le coeur.
       this.addLikes();
    }
-   
+
    //Création element Image
-   getTemplateImage(x){
+   getTemplateImage(x) {
       let template = ` 
                   <figure class="media">
                      <img src="./public/images/${x.photographerId}/${x.image}" alt="${x.alt}"/>
@@ -99,17 +101,15 @@ export default class PhotographerPage {
                         <div class="totalLikes" aria-label="likes" title="J'aime" data-like="0">
                            <span>${x.likes}</span>
                            <i class="far fa-heart"></i>
-
                         </div>   
                      </figcaption>
                   </figure>
       `;
-
       return template;
    }
 
    //Création element Viodéo
-   getTemplateVideo(x){
+   getTemplateVideo(x) {
       let template = ` 
                   <video class="media" controls  poster=""><source src="./public/images/${x.photographerId}/${x.video}" /></video>
                   <div class="media-footer">  
@@ -124,28 +124,28 @@ export default class PhotographerPage {
    }
 
    //permet de recuperer le id (dans l'url)d'un photographre.
-   getPhotographerId(){
+   getPhotographerId() {
       let objetId = window.location.href.split("id=");
       let ident = objetId[1];
-      
+
       return ident;
    }
 
-//ajouter un like
-   addLikes(){
+   //ajouter un like
+   addLikes() {
       let totalLikes = document.querySelectorAll(".totalLikes");
-      
-      totalLikes.forEach(currentElt =>{
-         currentElt.addEventListener("click",event => {
-            
+
+      totalLikes.forEach(currentElt => {
+         currentElt.addEventListener("click", event => {
+
 
             let eltNbreLike = currentElt.querySelector("span");
             //eltNbreLike[1].textContent :je selectionne le deuxieme child./ 
             let currentTotalLike = parseInt(eltNbreLike.textContent);
-            
+
             let stateLike = currentElt.getAttribute("data-like");
             let iconeCoeur = currentElt.querySelector("i")
-            if(stateLike == 0){
+            if (stateLike == 0) {
                currentElt.setAttribute("data-like", 1);
                currentTotalLike++;
                currentElt.setAttribute("title", "Je n'aime pas");
@@ -154,18 +154,18 @@ export default class PhotographerPage {
                iconeCoeur.classList.value = "fas fa-heart";
 
                //annuler le premier click (le deuxieme click).
-            }else {
-               currentElt.setAttribute("data-like", 0);  
+            } else {
+               currentElt.setAttribute("data-like", 0);
                currentTotalLike--;
                currentElt.setAttribute("title", "J'aime");
                iconeCoeur.classList.value = "far fa-heart";
 
             }
-            
-            eltNbreLike.textContent = currentTotalLike; 
+
+            eltNbreLike.textContent = currentTotalLike;
 
          })
-      })   
+      })
    }
 }
 
